5069607e0c7ef58e4c1398f284b2429ddf7558bf
Florian Moser <git@famoser.ch>
2025-12-26 18:49:34 +0100

    ref: Move to string GUIDs

--- src/Entity/Traits/IdTrait.php
+++ src/Entity/Traits/IdTrait.php
@@ -11,8 +11,8 @@
 
 namespace App\Entity\Traits;
 
+use Doctrine\DBAL\Types\Types;
 use Doctrine\ORM\Mapping as ORM;
-use Symfony\Bridge\Doctrine\Types\UuidType;
 
 /*
  * the id used in the entities
@@ -24,7 +24,7 @@ trait IdTrait
      * @var string|null will be null when not inserted into the db yet
      */
     #[ORM\Id]
-    #[ORM\Column(type: UuidType::NAME, unique: true)]
+    #[ORM\Column(type: Types::GUID, unique: true)]
     #[ORM\GeneratedValue(strategy: 'CUSTOM')]
     #[ORM\CustomIdGenerator(class: 'doctrine.uuid_generator')]
     private ?string $id = null;
