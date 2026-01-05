1ec8440dee3585811fd6d47437604fc05b6f6831
Florian Moser <git@famoser.ch>
2026-01-05 08:54:04 +0100

    ref: Refactor animal keeper form to use components

---
+++ assets/vue/components/Form/Shared/ThingComponent.vue
@@ -0,0 +1,39 @@
+<template>
+  <form-field for-id="name" :label="label" :field="fields.name">
+    <text-area id="name" :field="fields.name" v-model="entity.name"
+               @blur="blurField('name')" @update:modelValue="validateField('name')"/>
+  </form-field>
+</template>
+
+<script>
+import {templatedForm, createField, requiredRule} from '../utils/form'
+import FormField from '../../Library/FormLayout/FormField'
+import TextArea from "../../Library/FormInput/TextArea.vue";
+
+export default {
+  emits: ['update'],
+  components: {
+    TextArea,
+    FormField
+  },
+  props: {
+    label: {
+      type: String,
+      required: true
+    }
+  },
+  mixins: [templatedForm],
+  data() {
+    return {
+      fields: {
+        name: createField(requiredRule),
+      },
+      entity: {
+        name: null,
+      }
+    }
+  }
+}
+
+export const thingFields = ["name"]
+</script>
