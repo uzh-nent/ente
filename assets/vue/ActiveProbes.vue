9ebac1c02685dd840862db7e3fd74a91f176b593
Florian Moser <git@famoser.ch>
2026-02-06 17:21:43 +0100

    ref: Remove unused imports

--- assets/vue/ActiveProbes.vue
+++ assets/vue/ActiveProbes.vue
@@ -17,34 +17,13 @@
 
 <script>
 
-import {api, preloadApi, router} from './services/api'
-import {displaySuccess} from './services/notifiers'
-import LoopingRhombusSpinner from './components/Library/View/Base/LoopingRhombusSpinner.vue'
-import ButtonConfirmModal from './components/Library/Behaviour/Modal/ButtonConfirmModal.vue'
-import ServiceRequestForm from "./components/Form/Probe/ServiceRequestForm.vue";
-import moment from "moment";
-import SpecimenMetaForm from "./components/Form/Probe/SpecimenMetaForm.vue";
-import OwnerForm from "./components/Form/Probe/OwnerForm.vue";
-import FindPatientForm from "./components/Form/Probe/FindPatientForm.vue";
-import ServiceTimeForm from "./components/Form/Probe/ServiceTimeForm.vue";
-import {probeConverter} from "./services/domain/converters";
-import OrdererOrgForm from "./components/Form/Probe/OrdererOrgForm.vue";
-import OrdererPracForm from "./components/Form/Probe/OrdererPracForm.vue";
+import {preloadApi} from './services/api'
 import ProbeNavigationView from "./components/View/ProbeNavigationView.vue";
 
 export default {
   emits: ['added'],
   components: {
     ProbeNavigationView,
-    OrdererPracForm,
-    OrdererOrgForm,
-    ServiceTimeForm,
-    OwnerForm,
-    FindPatientForm,
-    SpecimenMetaForm,
-    ServiceRequestForm,
-    ButtonConfirmModal,
-    LoopingRhombusSpinner,
   },
   data() {
     return {
