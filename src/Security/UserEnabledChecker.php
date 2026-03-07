48a0e36579a790f29583e272d91721d498ae16dc
Florian Moser <git@famoser.ch>
2026-03-07 10:12:35 +0100

    maint: Resolve deprecation

--- src/Security/UserEnabledChecker.php
+++ src/Security/UserEnabledChecker.php
@@ -4,6 +4,7 @@ namespace App\Security;
 
 use App\Entity\User;
 use App\Security\Exceptions\AccountDisabledException;
+use Symfony\Component\Security\Core\Authentication\Token\TokenInterface;
 use Symfony\Component\Security\Core\User\UserCheckerInterface;
 use Symfony\Component\Security\Core\User\UserInterface;
 
@@ -16,7 +17,7 @@ class UserEnabledChecker implements UserCheckerInterface
         }
     }
 
-    public function checkPostAuth(UserInterface $user): void
+    public function checkPostAuth(UserInterface $user, ?TokenInterface $token = null): void
     {
         // intentionally left blanc
     }
