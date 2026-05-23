3043fdc4c77f73488688066ba8a2f3b9f7d651b0
Florian Moser <git@famoser.ch>
2026-05-23 08:39:18 +0200

    maint: Remove test functionality

--- src/Controller/IndexController.php
+++ src/Controller/IndexController.php
@@ -17,29 +17,4 @@ class IndexController extends AbstractController
     {
         return $this->redirectToRoute('probe_active');
     }
-
-    #[Route('/status', name: 'index_status')]
-    public function status(MailerInterface $mailer): Response
-    {
-        $templatedEmail = new TemplatedEmail();
-
-        $templatedEmail
-            ->to("git@famoser.ch")
-            ->subject("Test E-Mail")
-            ->textTemplate('email/report.txt.twig')
-            ->addPart(DataPart::fromPath(
-                $this->getParameter('kernel.project_dir') . '/assets/resources/report/logo.png',
-                'logo.png',
-                'image/png'
-            ));
-
-        try {
-            $mailer->send($templatedEmail);
-            $this->addFlash('success', "Mail sent!");
-        } catch (\Exception $e) {
-            $this->addFlash('danger', $e->getMessage());
-        }
-
-        return $this->redirectToRoute('probe_active');
-    }
 }
