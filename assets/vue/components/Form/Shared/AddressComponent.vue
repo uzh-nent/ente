126af425bb1587682767468c777a5e80b17a6267
Florian Moser <git@famoser.ch>
2026-09-01 09:51:37 +0200

    feat: Allow to change proposed city with arrow keys

--- assets/vue/components/Form/Shared/AddressComponent.vue
+++ assets/vue/components/Form/Shared/AddressComponent.vue
@@ -9,7 +9,8 @@
     <div class="col-md-3">
       <form-field for-id="postalCode" :label="$t('address.postal_code')" :field="fields.postalCode">
         <text-input id="postalCode" type="text" :field="fields.postalCode" v-model="entity.postalCode"
-                    @blur="blurField('postalCode')" @update:modelValue="validateField('postalCode')"/>
+                    @blur="blurField('postalCode')" @update:modelValue="validateField('postalCode')"
+                    @focusin="postalCodeFocus = true" @focusout="postalCodeFocus = false" @keydown="handlePostalCodeKeydown"/>
       </form-field>
     </div>
     <div class="col-md-7">
@@ -17,6 +18,11 @@
         <text-input id="city" :field="fields.city" v-model="entity.city"
                     @blur="blurField('city')" @update:modelValue="validateField('city')"/>
       </form-field>
+    </div>
+    <div class="col-md-12">
+      <span v-if="showCityHint" class="form-text d-block shift-up mb-3">
+        {{ $t('_form.use_arrow_keys_to_change_city', {current:suggestedCity+1, total:matchingCities.length}) }}
+      </span>
     </div>
     <div class="col-md-12">
       <form-field for-id="addressLines" :label="$t('address.address_lines')" :field="fields.addressLines">
@@ -45,6 +51,7 @@ export default {
   data() {
     return {
       postalCodeFocus: false,
+      suggestedCity: 0,
       fields: {
         addressLines: createField(),
         countryCode: createField(countryCode),
@@ -66,10 +73,46 @@ export default {
       }
 
       const numberPostalCode = Number(this.entity.postalCode)
-      return postalCodes.filter(entry => entry.pc === numberPostalCode)?.c
+      return postalCodes.filter(entry => entry.pc === numberPostalCode).map(c => c.c)
+    },
+    showCityHint: function () {
+      return this.postalCodeFocus && this.matchingCities.length > 1
+    }
+  },
+  watch: {
+    matchingCities: function () {
+      if (this.postalCodeFocus) {
+        if (this.matchingCities.length > 0) {
+          this.entity.city = this.matchingCities[0]
+        }
+      }
+    }
+  },
+  methods: {
+    handlePostalCodeKeydown: function (event) {
+      if (!this.showCityHint) {
+        return
+      }
+
+      if (event.key === 'ArrowDown') {
+        this.suggestedCity = Math.min(
+            this.suggestedCity + 1,
+            this.matchingCities.length - 1
+        )
+        this.entity.city = this.matchingCities[this.suggestedCity]
+      } else if (event.key === 'ArrowUp') {
+        this.suggestedCity = Math.max(this.suggestedCity - 1, 0)
+        this.entity.city = this.matchingCities[this.suggestedCity]
+      }
     }
   }
 }
 
 export const addressFields = ["addressLines", "countryCode", "postalCode", "city"]
 </script>
+
+<style scoped>
+.shift-up {
+  margin-top: -0.5rem;
+}
+</style>
